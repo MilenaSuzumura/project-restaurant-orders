1c419e7ccb59d19c20b61dcc15783616c6c2426e
Milena Suzumura <milena.suzumura11@gmail.com>
2023-05-23 14:33:03 -0300

    requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredientOne = Ingredient('manteiga')
+
+    assert ingredientOne.name == 'manteiga'
+
+    assert ingredientOne.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED
+    }
+    assert ingredientOne.restrictions != {Restriction.LACTOSE}
+
+    assert ingredientOne.__repr__() == "Ingredient('manteiga')"
+    assert ingredientOne.__repr__() != "Ingredient('farinha')"
+
+    assert ingredientOne.__eq__(Ingredient('manteiga')) is True
+    assert ingredientOne.__eq__(Ingredient('farinha')) is not True
+
+    assert ingredientOne.__hash__() != hash('farinha')
+    assert ingredientOne.__hash__() == hash('manteiga')
