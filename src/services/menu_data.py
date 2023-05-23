4db6326ac29de48010d211f2f174c828cf948910
Milena Suzumura <milena.suzumura11@gmail.com>
2023-05-23 16:00:54 -0300

    sei lá oque está errado no requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,23 @@
+import pandas as pd
+from models.dish import Dish, Ingredient
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self._data = pd.read_csv(source_path)
+        new_dishes = {}
+
+        for dish, price, ingredient, recipe_amount in self._data.itertuples(
+            index=False
+        ):
+            if dish not in new_dishes.keys():
+                new_dishes[dish] = Dish(dish, price)
+
+            else:
+                new_dishes[dish].add_ingredient_dependency(
+                    Ingredient(ingredient),
+                    recipe_amount
+                )
+
+        self.dishes = set(new_dishes.values())
